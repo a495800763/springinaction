8c9c9ec56f8b5105ff30a5733183d76fced67c54
a495800763 <495800763@qq.com>
2020-10-18 16:44:59 +0800

    dasdasdasdasd

---
+++ src/main/java/com/liumq/spittr/SpittrApplication.java
@@ -0,0 +1,13 @@
+package com.liumq.spittr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpittrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpittrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/liumq/spittr/SpittrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liumq.spittr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpittrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
